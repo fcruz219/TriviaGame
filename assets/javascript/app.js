3fd9c8c0ee765194e97389ced5c3ec6727bd560a
Francisco <fcruz219@gmail.com>
2019-03-05 22:10:19 -0500

    finished adding all the questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+var questions = [{
+    question: "How did Daenerys Targaryen eventually hatch her eggs?",
+    answerList: ["In a lighting storm", "In a funeral pyre", "In a fireplace", "In a frozen cave"],
+    answer: 1
+},{
+    question: "Valar Morghulis translates to:",
+    answerList: ["All men must live", "Death waits for no man", "All men must serve", "All men must die"],
+    answer: 3
+},{
+    question: "Besides obsidian, what is the only other substance capable of defeating the White Walkers?",
+    answerList: ["Weirwood", "Wildfire", "Valyrian Steel", "Ice"],
+    answer: 2
+}, {
+    question: "Actor Jacob Anderson, who plays Grey Worm, is not only a famous actor but also a:",
+    answerList: ["Singer and songwriter", "Artist", "Athlete", "Writer"],
+    answer: 0
+},{
+    question: "Which of the Stark family direwolves was killed in retaliation for an attack on Prince Joffrey?",
+    answerList: ["Ghost", "Lady", "Nymeria", "Summer"],
+    answer: 1
+},{ 
+    question: "Arya's punishment for stealing from the Many-Faced God is:",
+    answerList: ["Death", "Memory Loss", "Blindness", "Paralysis"],
+    answer: 2
+},{
+    question: "The name of Tommens cat is:",
+    answerList: ["Little Lion", "Ser Pounce", "Prince Fuzz", "Shaggy Cat"],
+    answer: 1
+},{
+    question: "What is the name of Ned Stark's greatsword?",
+    answerList: ["Ice", "Oathkeeper", "Widow's Wail", "Northguard"],
+    answer:0
+},{
+    question: "Dead creatures ressurected by White Walkers are known as:",
+    answerList: ["Walkers", "Wights", "Zombies", "Claws"],
+    answer:1
+},{
+    question: "Who is known as the King Beyond The Wall",
+    answerList: ["The Night King", "Stannis Baratheon", "Tormund Giantsbane", "Mance Rayder"],
+    answer: 3
+}]
